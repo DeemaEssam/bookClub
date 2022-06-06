81d763fb2a971299126495b7a49453a8b7e224bf
Deema Essam <106381596+DeemaEssam@users.noreply.github.com>
2022-06-06 23:25:57 +0300

    Update Taken.cc

--- Taken.cc
+++ Taken.cc
@@ -38,7 +38,7 @@ void Taken::print(){
   cout << left << setw(columnWidth) << setfill(separator) << ptrclubMember->getfirstName();//get member's firstname
   cout << left << setw(columnWidth) << setfill(separator) << ptrclubMember->getlastName();//get member's lastname
   cout << left << setw(columnWidth) << setfill(separator) << ptrBook->getbookID();
-  cout << left << setw(columnWidth) << setfill(separator) << ptrBook->getbookTitle();
+  cout << left << setw(columnWidth+8) << setfill(separator) << ptrBook->getbookTitle();
   cout << left << setw(columnWidth) << setfill(separator) << ptrBook->getbookAuthor();
   cout << left << setw(columnWidth) << setfill(separator) << ptrBook->getbookYear();
 }
